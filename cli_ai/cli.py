f9507a7852dd42552d3e895e4afb343f19a6510f
Waldoh <waldoh@gptcoder.pro>
2023-10-16 19:13:44 -0300

    added prompt toolkit for cli interface

--- cli_ai/cli.py
+++ cli_ai/cli.py
@@ -1,16 +1,47 @@
+from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.styles import Style
 from core.OutputFormatter import OutputFormatter
 from core.CommandRunner import CommandRunner
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+# Define style for prompt_toolkit
+style = Style.from_dict(
+    {
+        "username": "#44ff44",
+        "at": "#ffffff",
+        "colon": "#ffffff",
+        "path": "#ff44ff",
+        "pound": "#ffffff",
+    }
+)
+
+
+def get_custom_prompt():
+    username = os.getenv("USER") or os.getenv("USERNAME")
+    pwd = os.getcwd()
+    return [
+        ("class:username", f"{username}"),
+        ("class:at", "@"),
+        ("class:colon", "python-cli "),
+        ("class:path", f"{pwd}"),
+        ("class:pound", "$ "),
+    ]
 
 
 def main():
     runner = CommandRunner()
 
+    # Initialize prompt_toolkit session and autocompleter
+    session = PromptSession()
+    completer = WordCompleter(["exit"], ignore_case=True)
+
     while True:
         try:
-            command = input(OutputFormatter.get_custom_prompt())
+            command = session.prompt(get_custom_prompt(), style=style)
 
             if command.lower() == "exit":
                 break
